f7a2cd836808d5cde97d255ebf114a31d8adcc89
Jean-Philippe BOURGEON <3356641+jpbourgeon@users.noreply.github.com>
2022-04-14 15:09:46 +0200

    Bump GH CLI v2.8.0

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,6 +3,6 @@
 
 cd /home/node
 
-## GH CLI v1.9.2
-wget -nc https://github.com/cli/cli/releases/download/v1.9.2/gh_1.9.2_linux_amd64.deb
-dpkg -i gh_1.9.2_linux_amd64.deb
+## GH CLI v2.8.0
+wget -nc https://github.com/cli/cli/releases/download/v2.8.0/gh_2.8.0_linux_amd64.deb
+dpkg -i gh_2.8.0_linux_amd64.deb
