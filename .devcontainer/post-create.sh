4cfed239007f35e3caa06da055e40ed75508ee9f
jpbourgeon <3356641+jpbourgeon@users.noreply.github.com>
2022-09-26 04:07:45 +0000

    feat: configure `rebase` as git default pull strategy

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,4 +1,3 @@
-
 #!/bin/sh
 
 cd /home/node
@@ -6,3 +5,6 @@ cd /home/node
 ## GH CLI v2.9.0
 wget -nc https://github.com/cli/cli/releases/download/v2.9.0/gh_2.9.0_linux_amd64.deb
 dpkg -i gh_2.9.0_linux_amd64.deb
+
+## Git config
+git config --global pull.rebase true
